0d970b100d337fa9c5521b1678202c8da8ed529f
Tyler Briskie <tylerbriskie@gmail.com>
2016-11-09 15:24:34 -0700

    checkerboard pattern laid out and colored

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// Your JS goes here
+$(document).ready(function() {
+
+  populateCheckerBoard();
+  colorBoard();
+
+
+
+
+  function populateCheckerBoard() {
+      for (var i = 0; i < 8; i++) {
+          $('body').append('<div class="row"></div>');
+          for (var j = 0; j < 8; j++) {
+              if ((j + i + 10) % 2 === 0) {
+                  $('.row:last-child').append('<div class="black"></div>');
+              } else {
+                  $('.row:last-child').append('<div class="red"></div>');
+              }
+          }
+      }
+  };
+
+  function colorBoard(){
+    $('.black').css('background-color','black').css('width','12%').css('float','left').css('paddingBottom','12%');
+    $('.red').css('background-color','red').css('width','12%').css('float','left').css('paddingBottom','12%');
+  };
+
+});
